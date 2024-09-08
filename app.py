5c43cf1b0cfcdbbaf91f2be86158935060c781f1
Areeba Shakeel <shakeelareeba02@gmail.com>
2024-09-08 20:22:16 +0530

    analysis

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 
 def load_dataset():
-    df=pd.read_csv(r'C:\Users\LENOVO\Desktop\Video-geames-project\Dataset\vgsales.csv')
+    df=pd.read_csv(r'C:\Users\LENOVO\Documents\Video-Game-Analysis\Dataset\vgsales.csv')
     df.Publisher.fillna('Electronic Arts',inplace=True)
     df.Year.fillna(2009.0,inplace=True)
     return df
@@ -16,6 +16,30 @@ def load_dataset():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/overview')
+def overview():
+    return render_template('overview.html')
+
+@app.route('/platform')
+def platform():
+    return render_template('platform.html')
+
+@app.route('/Publisher')
+def publisher():
+    return render_template('Publisher.html')
+
+@app.route('/sales')
+def sales():
+    return render_template('sales.html')
+
+@app.route('/summary')
+def summary():
+    return render_template('summary.html')
+
+@app.route('/tool')
+def tools():
+    return render_template('tool.html')
 
 
 if __name__ == '__main__':
